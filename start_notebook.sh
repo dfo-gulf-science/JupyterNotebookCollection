80e41b9565663ce483c8b8fffa56bc0a0d1c416c
david fishman <david.fishman@dfo-mpo.gc.ca>
2023-11-23 17:24:31 +0000

    updating script

--- start_notebook.sh
+++ start_notebook.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-git fetch origin
-git reset --hard origin/main
+#git fetch origin
+#git reset --hard origin/main
 
-jupyter notebook
+#jupyter notebook --ip='*' --NotebookApp.token='' --NotebookApp.password='' -p 80:80
+
+
+jupyter notebook --port=80 --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.notebook_dir='/opt/JupyterNotebookCollection/'
